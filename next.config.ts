8dac38d98fd03845288d7a63385d641ce1a3100c
Gabriel pena santos <butz@MacBook-Air-de-Gabriel.local>
2026-03-31 17:34:46 -0300

    configura proxy da api na vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.18.120'],
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://159.223.111.33:8000/api/:path*',
+      },
+      {
+        source: '/produtos/:path*',
+        destination: 'http://159.223.111.33:8000/produtos/:path*',
+      },
+      {
+        source: '/static/:path*',
+        destination: 'http://159.223.111.33:8000/static/:path*',
+      },
+      {
+        source: '/media/:path*',
+        destination: 'http://159.223.111.33:8000/media/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
